9722124bb3f779e6c001b11a446151bb06e6f7a8
tomasz.wnukn <tomasz.wnuk@medicover.pl>
2024-01-30 16:25:52 +0100

    feat(root): tailwind fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}",
+    "./apps/*/src/app/**/*.{html,ts}",
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
