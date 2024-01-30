75d1fa6a78550f95bb6eb8ea8fdc152fba3c3396
tomasz.wnukn <tomasz.wnuk@medicover.pl>
2024-01-30 19:17:35 +0100

    feat(layout): migrate to libs

---
+++ libs/core/src/lib/core/interceptors/preloader.interceptor.ts
@@ -0,0 +1,37 @@
+import { Injectable, Provider } from '@angular/core';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HTTP_INTERCEPTORS,
+} from '@angular/common/http';
+
+import { Observable, finalize } from 'rxjs';
+import { PreloaderService } from '@skoer-hr-solutions/data-access';
+
+@Injectable()
+class PreloaderInterceptor implements HttpInterceptor {
+  constructor(private preloaderService: PreloaderService) {}
+
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    this.preloaderService.show();
+
+    return next.handle(request).pipe(
+      finalize(() => {
+        this.preloaderService.hide();
+      })
+    );
+  }
+}
+
+export function providePreloaderInterceptor(): Provider {
+  return {
+    provide: HTTP_INTERCEPTORS,
+    useClass: PreloaderInterceptor,
+    multi: true,
+  };
+}
